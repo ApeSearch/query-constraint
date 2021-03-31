26d99069e8570bf8f5cd02e327f51aa4f284574d
xiongrob <62448777+xiongrob@users.noreply.github.com>
2021-03-31 13:08:33 -0400

    Add structure for Index and remove circular dependencies

--- include/Index.h
+++ include/Index.h
@@ -3,12 +3,41 @@
 
 #ifndef INDEX_H_AS
 #define INDEX_H_AS
+#include "Token.h"
 
+/*
+ * 
+ * The Index will now be a hash table like structure that ISRs can only interact with methods that
+ *  expose the functionality of the Index. That is, perhaps having a pointer to a posting list, you can
+ *  basically use that to find the next post in a posting list.
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 typedef size_t Location; // The numbering of a token
 typedef size_t FileOffset;
 
+class SynchronizationEntry
+   {
+    size_t hightBitOfSeekLoc; // Relative to a posting list ( ordered )
+    size_t seekOffset;  // Relative to the start of the posting list.
+    size_t absoluteLoc; // Relative to every posting list.
+   };
+
+class Post;
+
 class PostingList
     {
+    // Represent the syncrhonoziation table
+    size_t numberOfBytes; // size of posting list
+    std::vector<SynchronizationEntry> synchTable;
+    size_t numberOfPosts;   // Basically the number of occurnces of a particular token.
+    size_t numOfDocs; // number of documents that contain this info.
+    TokenType type; // Type of token ( be it eod, anchor text, url, tile, or body)
     public:
         virtual Post *Seek( Location l ); //add up deltas until location reached
 
@@ -84,7 +113,6 @@ class IndexHT
     class Dictionary
         {
             public:
-                ISR *OpenISR(char token);
                 Location GetNumberOfWords();
                 Location GetNumberOfUniqueWords();
                 Location GetNumberOfDocuments();
@@ -97,9 +125,9 @@ class IndexHT
                  LocationsInIndex,
                  MaximumLocation;
 
-        Index() :  chunk(), WordsInIndex(0), DocumentsInIndex(0), LocationsInIndex(0), MaximumLocation(0) {}
+        IndexHT() :  WordsInIndex(0), DocumentsInIndex(0), LocationsInIndex(0), MaximumLocation(0) {}
 
-        std::pair<Location, Post *> findPost(  )
+        std::pair<Location, Post *> findPost(  );
         Post *goToNext( Location location ); // May need to inherit here...
 
 
