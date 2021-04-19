708e7b2a1f2d06b781752556e358e7ea0ee82362
Paul Zhang <PaulZhang12@outlook.com>
2021-04-19 12:19:01 -0400

    Added Static stats

--- include/Ranker.h
+++ include/Ranker.h
@@ -14,6 +14,23 @@ static const struct DynamicStats
     static const size_t W_AllWordsFrequent = 5;
     static const size_t W_MostWordsFrequent = 5;
     static const size_t W_SomeWordsFrequent = 5;
+
+    static const size_t W_Anchor = 20;
+    static const size_t W_Url = 10;
+    static const size_t W_Title = 10;
+    static const size_t W_Body = 1;
+    };
+
+static const struct StaticStats
+    {
+    static const size_t ShortTitleThreshold = 20;
+    static const size_t MinNumAnchorText = 10;
+    static const size_t MaxURLLengthToBeShort = 20;
+
+    static const size_t W_Domain = 2;
+    static const size_t W_ShortTitle = 2;
+    static const size_t W_LotsAnchorText = 10;
+    static const size_t W_ShortURL = 2;
     };
 
 
